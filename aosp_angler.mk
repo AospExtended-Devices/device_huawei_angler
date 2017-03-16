182c30c321458496f5ae215701e4bf3cf7cdbfd4
Evan Anderson <evananderson@aospa.co>
2017-03-16 02:13:58 -0400

    angler: aosp_angler: call device-system vendor file
    * device-system.mk handles proprietary blobs that reside in the system
    directory
    
    Change-Id: I8bafc6d9079e6680a17f305573c2253cb00946b6
    Signed-off-by: Evan Anderson <evananderson@aospa.co>

--- aosp_angler.mk
+++ aosp_angler.mk
@@ -34,7 +34,8 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 #PRODUCT_COPY_FILES += device/huawei/angler/fstab.aosp_angler:root/fstab.angler
 
 $(call inherit-product, device/huawei/angler/device.mk)
-$(call inherit-product-if-exists, vendor/huawei/angler/angler-vendor.mk)
+$(call inherit-product-if-exists, vendor/huawei/angler/device-vendor.mk)
+$(call inherit-product-if-exists, vendor/huawei/angler/device-system.mk)
 $(call inherit-product, vendor/aosp/common.mk)
 
 # Boot animation
