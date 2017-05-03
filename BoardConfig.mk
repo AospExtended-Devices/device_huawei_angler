6cb7360df7e0c04a106ce6c67215d5dc62e6911f
nickd81 <nickdoh81@gmail.com>
2017-05-03 11:23:26 -0400

    Use device audio_effects.conf
    - This will ignore the audio_effects.conf from the vendor partition.
    -- Retrofit to the DSP effects
    --- Also, add the Google Assistant prop back
    
    Change-Id: I87d1b3b5e0af999eaaf138a0c64cea7d19041b77
    Signed-off-by: FireLord1 <firelord.xda@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -167,4 +167,8 @@ BOARD_HARDWARE_CLASS := \
     hardware/cyanogen/cmhw \
     device/huawei/angler/cmhw
 
+# Audio effects for DSP Manager
+TARGET_USE_DEVICE_AUDIO_EFFECTS_CONF := true
+TARGET_IGNORE_VENDOR_AUDIO_EFFECTS_CONF := true
+
 -include vendor/huawei/angler/BoardConfigVendor.mk
