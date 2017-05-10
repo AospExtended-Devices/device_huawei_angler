4c6cb2c436c87e6a66a479f78077a96d255fad8e
FireLord1 <firelord.xda@gmail.com>
2017-05-10 10:30:33 +0530

    angler: add build fingerprint & description from lineage.mk
    Signed-off-by: FireLord1 <firelord.xda@gmail.com>

--- aosp_angler.mk
+++ aosp_angler.mk
@@ -46,3 +46,9 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     AOSPLinks
+
+# Override build properties.
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=angler \
+    BUILD_FINGERPRINT=google/angler/angler:7.1.2/N2G47O/3852959:user/release-keys \
+    PRIVATE_BUILD_DESC="angler-user 7.1.2 N2G47O 3852959 release-keys"
